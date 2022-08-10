7f86097266fd99df9309f1e591e8f178a59e817e
NB-21122703\NB-000000 <donghwan.kim@bankwareglobal.com>
2022-08-11 00:24:15 +0900

    commit Test

---
+++ src/main/java/gitTest/ContorllerResource.java
@@ -0,0 +1,19 @@
+package gitTest;
+
+import bxm.common.annotaion.BxmCategory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ *
+ * @author sysadmin
+ */
+@RestController
+@RequestMapping("/")
+@BxmCategory(logicalName="컨트롤러 리소스", description="")
+public class ContorllerResource {
+	private Logger logger= LoggerFactory.getLogger(getClass());
+}
